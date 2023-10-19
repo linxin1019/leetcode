fdf7e1e2cd14f91384ac5ca0a57d86a6d706bd8c
linxin1019 <linxin1019@hotmail.com>
2023-10-19 22:56:31 +0900

    MajorityElement

---
+++ src/main/java/org/top150/MajorityElement.java
@@ -0,0 +1,54 @@
+package org.top150;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array nums of size n, return the majority element.
+ *
+ * The majority element is the element that appears more than ⌊n / 2⌋ times.
+ * You may assume that the majority element always exists in the array.
+ */
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i: nums) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        int n = nums.length / 2;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n) {
+                return entry.getKey();
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * The intuition behind the Moore's Voting Algorithm is based on the fact that if there is a majority element in an array
+     * , it will always remain in the lead, even after encountering other elements.
+     */
+    public int mooreVoting(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
